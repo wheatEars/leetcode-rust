39e784eab138422862266201991105d0d591b9f9
WheatEars <1460592649@qq.com>
2025-06-16 15:53:54 +0800

    init

--- src/solution/s0005_longest_palindromic_substring.rs
+++ src/solution/s0005_longest_palindromic_substring.rs
@@ -1,35 +1,25 @@
 /**
- * [5] Longest Palindromic Substring
+ * [0005] Longest Palindromic Substring
  *
- * Given a string s, return the longest palindromic substring in s.
+ * Given a string s, return the longest <span data-keyword="palindromic-string">palindromic</span> <span data-keyword="substring-nonempty">substring</span> in s.
  *  
  * Example 1:
- *
+ * 
  * Input: s = "babad"
  * Output: "bab"
- * Note: "aba" is also a valid answer.
- *
+ * Explanation: "aba" is also a valid answer.
+ * 
  * Example 2:
- *
+ * 
  * Input: s = "cbbd"
  * Output: "bb"
- *
- * Example 3:
- *
- * Input: s = "a"
- * Output: "a"
- *
- * Example 4:
- *
- * Input: s = "ac"
- * Output: "a"
- *
+ * 
  *  
  * Constraints:
- *
+ * 
  * 	1 <= s.length <= 1000
- * 	s consist of only digits and English letters (lower-case and/or upper-case),
- *
+ * 	s consist of only digits and English letters.
+ * 
  */
 pub struct Solution {}
 
@@ -40,36 +30,32 @@ pub struct Solution {}
 
 impl Solution {
     pub fn longest_palindrome(s: String) -> String {
-        let chars: Vec<char> = s.chars().collect();
-        if chars.len() < 2 {
-            return s;
+        let mut dp: Vec<Vec<bool>> = vec![vec![false; s.len()]; s.len()];
+        let len = s.len();
+        let s = s.as_bytes();
+        
+        for i in 0..len {
+            dp[i][i] = true; // single character is a palindrome
         }
 
-        let mut begin = 0;
-        let mut end = 0;
-
-        for s in 0..chars.len() {
-            for e in s + 1..chars.len() {
-                if Self::is_palindrome(&chars, s, e) && e - s > end - begin {
-                    end = e;
-                    begin = s;
+        for step in 1..len {
+            for i in 0..len - step {
+                if (s[i] == s[i + step]) && (step == 1 || dp[i + 1][i + step - 1]) {
+                    dp[i][i + step] = true;
                 }
             }
         }
-        s[begin..end + 1].into()
-    }
 
-    fn is_palindrome(s: &Vec<char>, begin: usize, end: usize) -> bool {
-        let mut end = end;
-        let mut begin = begin;
-        while begin < end {
-            if s[begin] != s[end] {
-                return false;
+        let mut max = ((0,0), 1);
+
+        for i in (0..s.len()).rev() {
+            for j in i + 1..s.len() {
+                if (dp[i][j] && j - i >= max.1) {
+                    max = ((i, j), j - i);
+                }
             }
-            begin += 1;
-            end -= 1;
         }
-        true
+        String::from_utf8(s[max.0.0..max.0.1 + 1].to_vec()).unwrap()
     }
 }
 
@@ -81,36 +67,10 @@ mod tests {
 
     #[test]
     fn test_0005_example_1() {
-        let s = "babad".to_string();
-
-        assert_eq!(Solution::longest_palindrome(s), "bab".to_string());
-    }
-
-    #[test]
-    fn test_0005_example_2() {
-        let s = "cbbd".to_string();
-
-        assert_eq!(Solution::longest_palindrome(s), "bb".to_string());
-    }
-
-    #[test]
-    fn test_0005_example_3() {
-        let s = "a".to_string();
-
-        assert_eq!(Solution::longest_palindrome(s), "a".to_string());
-    }
-
-    #[test]
-    fn test_0005_example_4() {
-        let s = "ac".to_string();
-
-        assert_eq!(Solution::longest_palindrome(s), "a".to_string());
-    }
-
-    #[test]
-    fn test_0005_empty_string() {
-        let s = String::new();
-
-        assert_eq!(Solution::longest_palindrome(s), String::new());
+        print!("{}",Solution::longest_palindrome("babad".to_string()));
+        assert!(Solution::longest_palindrome("babad".to_string()) == "bab".to_string());
+        assert!(Solution::longest_palindrome("cbbd".to_string()) == "bb".to_string());
+        assert!(Solution::longest_palindrome("aaaa".to_string()) == "aaaa".to_string());
+        assert!(Solution::longest_palindrome("bb".to_string()) == "bb".to_string());
     }
 }
